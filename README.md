64a7d4ea456a8ed5b114c7acf3cd0864aba09d33
Ridalord <ridhwan.alabi@gmail.com>
2022-12-08 18:38:16 +0100

    Readme file added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # web-dev-test
+# This is a frontend web development test for a job application at Kreatix.
+
+
+# This responsive site was built with HTML, CSS, and JavaScript incoperating the knowledge of HTML5, flexbox, grid layouts, DOM manipulations using javascript
+# Keeping in mind best web practices and standards.
+
+
+
+# This site is a landing page for a commercial organisation that distributes moniitor riser. There's a section that shows all available products
+# and there's a button that adds an item when clicked. On each card is a botten that removes a particular product card when clicked on
--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-document.getElementById('overlay').style.display = 'none';
-document.getElementById('nav-overlay').style.display = 'none';
-document.getElementById('4').style.display = 'none';
+//Making an array of the ids of the children(divs) elements of the parents (card section) 
+const parent = document.getElementById('parent');
+const children = Array.from(parent.children);
+const ids = children.map(element => {
+  return element.id;
+});
+//
 
-var value = document.getElementById('null').getAttribute('.null');
+document.getElementById('overlay').style.display = 'none'; //setting the display value the of pop-up overlay to none at default
+document.getElementById('nav-overlay').style.display = 'none';  //setting the display value of the navigation overlay to none at default
+document.getElementById('4').style.display = 'none';    //setting the display value of the extra card (4th card) to none at default
+
+//Block of codes to grey out the sold out card
+var value = document.getElementById('null').getAttribute('.null');  
 if (value === null) {
     document.querySelector('.null').style.opacity='0.5';
 }
 
-
+//The onclick event listener for the select reward button
 function getReward() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('close').addEventListener('click', function () {
                     document.getElementById('overlay').style.display = 'none';
         })
 }
+
+//The on click event listener for the drop down menubar
 function dropDown() {
     document.getElementById('nav-overlay').style.display = 'block';
     document.getElementById('nav-overlay').addEventListener('click', function () {
@@ -21,7 +32,7 @@ function dropDown() {
         })
 }
 
-// var x = document.getElementById("myLI").parentNode.nodeName;
+//The onclick event listener to remove a card 
 function remove(id) {
     if (id === "1") {
         document.getElementById('1').style.display= 'none'
@@ -38,14 +49,7 @@ function remove(id) {
 }
 
 
-
-const parent = document.getElementById('parent');
-
-const children = Array.from(parent.children);
-
-const ids = children.map(element => {
-  return element.id;
-});
+//Block of code to replace missing cards
 function add() {
     for (var i = 0; i < ids.length; i++){
         if (document.getElementById(ids[i]).style.display === 'none') {
